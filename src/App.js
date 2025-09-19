98c9d388e02b1552c4562c9c65c45b1cad6053ad
Aries Troy E. Cornelio <cornelioariestroy1998@gmail.com>
2025-09-20 01:32:56 +0800

    First commit - React content rating project of Mr. Aries Troy E. Cornelio, B.S Computer Science student.

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import ContentRating from "./ContentRating";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <ContentRating />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/ContentRating.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
+
+function ContentRating() {
+  const [likes, setLikes] = useState(0);
+  const [dislikes, setDislikes] = useState(0);
+
+  return (
+    <div className="text-center mt-5">
+      <h2>Content Rating Application</h2>
+      <p>Do you like this content?</p>
+      <div className="mt-3">
+        <button className="btn btn-success me-3" onClick={() => setLikes(likes + 1)}>
+          <i className="bi bi-hand-thumbs-up"></i> Like ({likes})
+        </button>
+        <button className="btn btn-danger" onClick={() => setDislikes(dislikes + 1)}>
+          <i className="bi bi-hand-thumbs-down"></i> Dislike ({dislikes})
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default ContentRating;
